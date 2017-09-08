675eecceb2f71c2e2582a43cda0ce0e186fd3b7e
[Austin Wade] <[aw11@protonmail.com]>
2017-09-07 23:09:01 -0700

    first

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+import hashlib
+
+def hashme(n):
+	return hashlib.sha256(n).hexdigest()
+
+
+def main():
+	# 0-255
+	VBidS = [187, 42, 83, 154, 172, 52, 98, 250, 7, 23]
+
+	GOVsN = ['a83', 'a93j', 'a983', 'a93', 'a9d8', 'f83', 'f7r', 'ghw', '28dj', 'fi4']
+
+	print "VoteBox Info 1:"
+	print VBidS
+
+	print "Gov Info 1:"
+	print GOVsN
+
+
+main()
